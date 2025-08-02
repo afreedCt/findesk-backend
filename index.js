88b875a566d16d4a8de594389877db6dc0a981a8
afreedCt <ctafreed278@gmail.com>
2025-08-02 14:47:27 +0530

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jsonserver=require('json-server')
+const cors = require('cors'); // ✅ add this
 
 const mpServer=jsonserver.create()
 // opening all middlewares (cors,etc)
@@ -8,6 +9,8 @@ const route=jsonserver.router('data.json')
 // PORT from webservice || 3000
 const PORT=process.env.PORT || 3000
 
+// ✅ Use CORS before middlewares
+mpServer.use(cors());
 // using all middleares
 mpServer.use(middleware)
 mpServer.use(route)
