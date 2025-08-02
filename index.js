cf6720f2bee204ec18a22c56ae2f02a82e508af1
afreedCt <ctafreed278@gmail.com>
2025-08-02 13:55:45 +0530

    backend completed to deploy in rebder

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonserver=require('json-server')
+
+const mpServer=jsonserver.create()
+// opening all middlewares (cors,etc)
+const middleware=jsonserver.defaults()
+// path to data.json(datbase)
+const route=jsonserver.router('data.json')
+// PORT from webservice || 3000
+const PORT=process.env.PORT || 3000
+
+// using all middleares
+mpServer.use(middleware)
+mpServer.use(route)
+
+// calling the server
+mpServer.listen(PORT,()=>console.log(`server running in ${PORT}`))
